1d6525816c9a013fc9eb657fe23b6c670a8cd21f
John Nganga <mukunajohn329@gmail.com>
2024-10-27 22:04:04 +0300

    chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from "./components/about/about.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { SpecialitiesComponent } from "./components/specialities/specialities.component";
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, HomeComponent, AboutComponent],
+  imports: [RouterOutlet, SidebarComponent, HomeComponent, 
+    AboutComponent, ExperienceComponent, SpecialitiesComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/specialities/specialities.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specialities',
+  standalone: true,
+  imports: [],
+  templateUrl: './specialities.component.html',
+  styleUrl: './specialities.component.css'
+})
+export class SpecialitiesComponent {
+
+}
